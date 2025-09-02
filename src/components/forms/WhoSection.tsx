7a56e1d120965bf6c6089c3c94784c418bc03410
staticWagomU <to6wa17@gmail.com>
2025-09-02 22:55:51 +0900

    feat: warningアイコンを置き換え

--- src/components/forms/WhoSection.tsx
+++ src/components/forms/WhoSection.tsx
@@ -99,17 +99,11 @@ export function WhoSection({ value, onChange, error }: WhoSectionProps) {
 					/>
 					<div className="p-3 bg-yellow-50 border border-yellow-200 rounded-md">
 						<div className="flex">
-							<svg
+							<img
+								src="/warning-box.svg"
+								alt="warning-box"
 								className="h-5 w-5 text-yellow-400 mr-2 flex-shrink-0"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-							>
-								<path
-									fillRule="evenodd"
-									d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-									clipRule="evenodd"
-								/>
-							</svg>
+							/>
 							<div className="text-sm text-yellow-800">
 								<p className="font-medium">注意</p>
 								<p>
