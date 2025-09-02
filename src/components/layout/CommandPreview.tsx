26155c5f223dac55fdcea7e170a7e7ca90b68cad
staticWagomU <to6wa17@gmail.com>
2025-09-02 21:57:35 +0900

    feat: コピーボタンのアイコンを置き替え

--- src/components/layout/CommandPreview.tsx
+++ src/components/layout/CommandPreview.tsx
@@ -70,34 +70,20 @@ export function CommandPreview({ config }: CommandPreviewProps) {
 				>
 					{copySuccess ? (
 						<>
-							<svg
-								className="w-4 h-4 mr-2"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-							>
-								<path
-									fillRule="evenodd"
-									d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-									clipRule="evenodd"
-								/>
-							</svg>
+							<img
+								src="/check.svg"
+								alt="calendar"
+								className="w-4 h-4 mr-2 text-white"
+							/>
 							コピーしました！
 						</>
 					) : (
 						<>
-							<svg
-								className="w-4 h-4 mr-2"
-								fill="none"
-								stroke="currentColor"
-								viewBox="0 0 24 24"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth={2}
-									d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-								/>
-							</svg>
+							<img
+								src="/copy.svg"
+								alt="calendar"
+								className="w-4 h-4 mr-2 text-white"
+							/>
 							コマンドをコピー
 						</>
 					)}
